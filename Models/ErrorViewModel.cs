a61e3707018c3edcb6fecdb84417e419463c1fef
shivanibalgoo1992 <87722022+shivanibalgoo1992@users.noreply.github.com>
2022-02-26 09:22:48 -0800

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace aspdotnetcore.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
